e34f695a0a262573fce7a58e3d802ce79702fbe0
RM0428MR <breakin@gmail.com>
2026-08-24 18:23:33 +0900

    Section6

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-import reactLogo from './assets/react.svg'
-import styles from './App.module.css'
-import { Button } from './components/Button'
-import { Profile } from './components/Profile'
+import reactLogo from "./assets/react.svg";
+import styles from "./App.module.css";
+import { Button } from "./components/Button";
+import { Profile } from "./components/Profile";
+import { Country } from "./components/Country";
+import { Card } from "./components/Card";
 
 export const App = () => {
   return (
     <div>
-      <img src={reactLogo} className={styles['react-logo']} />
+      <img src={reactLogo} className={styles["react-logo"]} />
       <h1>Hello React!</h1>
 
       {/* 以下に練習問題の回答を記述してください */}
+      <hr />
       <Button />
       <Button />
       <Button />
-
+      <hr />
       <Profile />
+      <hr />
+
+      <Card>
+        <Country info={{ name: "アメリカ", imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif' }} width={100} height={60} />
+      </Card>
     </div>
   );
-}
-
+};
